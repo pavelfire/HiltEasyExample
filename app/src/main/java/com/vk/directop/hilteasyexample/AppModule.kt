23a2b7d8700ed444fca7881672cafe1e1be8c905
PavelWrki5W10 <fire_ra@bk.ru>
2021-10-29 06:39:41 +0300

    dependencies added

---
+++ app/src/main/java/com/vk/directop/hilteasyexample/AppModule.kt
@@ -0,0 +1,17 @@
+package com.vk.directop.hilteasyexample
+
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTestString() = "This is a string we will inject"
+}
--- app/src/main/java/com/vk/directop/hilteasyexample/MainActivity.kt
+++ app/src/main/java/com/vk/directop/hilteasyexample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.vk.directop.hilteasyexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var testString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv1 = findViewById<TextView>(R.id.tv1)
+        tv1.text = testString
+
+        Log.d("MainActivity", "String from AppModule: $testString")
     }
 }
